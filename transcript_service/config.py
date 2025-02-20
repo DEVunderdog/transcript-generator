92b169db816e4203b6234980179e3b47220007d7
DEVunderdog <farhankhoja007@outlook.com>
2025-02-20 17:56:58 +0530

    transcript_service prod config

--- transcript_service/config.py
+++ transcript_service/config.py
@@ -2,9 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", case_sensitive=False)
+    model_config = SettingsConfigDict(case_sensitive=False)
 
-    service_account_key_path: str
     bucket_name: str
     project_id: str
     subscription_id: str
@@ -12,4 +11,4 @@ class Settings(BaseSettings):
     sender_password: str
 
 
-settings = Settings(_env_file="../.env/transcript_service.env")
+settings = Settings()
